ba0b5ac7c528250fd720f8f40eea241fa44848bf
SergeitZ <sergiofranco6@gmail.com>
2021-10-28 21:11:46 -0400

    Thursday 10/28/21 class

--- src/main/java/com/careerdevs/Intro/GreetingController.java
+++ src/main/java/com/careerdevs/Intro/GreetingController.java
@@ -13,21 +13,25 @@ public class GreetingController {
     private final AtomicLong counter = new AtomicLong();
     private final static String template = "Hello %s";
 
+    // root route || home route
     @GetMapping("/")
     public String getHello (){
         return "Hello World";
     }
 
+    // dummy data route | introduction to endpoint -> sayHello endpoint
     @GetMapping("/sayHello")
     public Greeting sayHello() {
         return new Greeting(42, "Hello CareerDevs!");
     }
 
+    // query parameters. localhost:8080/greeting?{value}={data}
     @GetMapping("/greeting")
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
 
+    // dynamic routing | url parameters
     @GetMapping("/hello/{name}")
     public Greeting hello(@PathVariable String name) {
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
---
+++ src/main/java/com/careerdevs/Intro/employees/EmployeeRepository.java
@@ -0,0 +1,5 @@
+package com.careerdevs.Intro.employees;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {}
